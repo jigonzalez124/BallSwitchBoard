edcee7f39b28226d7b354ff42d24e21899ee98a4
Ivan <jigonzalez124@gmail.com>
2015-09-17 09:31:14 -0400

    Added description

--- ballSwitchBoard.py
+++ ballSwitchBoard.py
@@ -1,5 +1,40 @@
-# you can use print for debugging purposes, e.g.
-# print "this is a debug message"
+'''
+File can be found here:  https://codility.com/demo/results/demo8AJJ26-ZHP/
+
+
+A board consisting of ball switches arranged into N rows and M columns is given. A ball switch is a
+device that can change the direction of a rolling ball. It has the following properties:
+
+-  it is square-shaped;
+-  it can be in one of three modes: −1, 0 or +1;
+-  a ball can enter it through the top or the left edge;
+-  a ball can leave it through the bottom or the right edge;
+-  if the mode is −1, a ball entering the device will leave it through the bottom edge;
+-  if the mode is +1, a ball entering the device will leave it through the right edge;
+-  if the mode is 0, a ball entering the device will not change direction;
+-  after a ball leaves the device, its mode is immediately negated.
+
+
+K balls are rolled through the board one by one. Each ball enters the board through the top edge of
+the top-left ball switch. Then it rolls through the board and possibly changes its direction
+depending on the modes of the switches through which it passes. Eventually it leaves the
+board either:
+
+-  through the bottom edge of one of the switches in the bottom row; or
+-  through the right edge of one of the switches in the rightmost column.
+
+
+
+Write a function:
+
+def solution(A, K)
+
+that,given a matrix A consisting of N rows and M columns describing a board of
+ball switches and a non-negative number K of balls inserted into this board,
+returns the number of balls that will leave the board through the bottom edge
+of the bottom-right switch.
+
+'''
 
 def solution(A, K):
     
